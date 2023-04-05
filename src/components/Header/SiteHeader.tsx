678920edfa5f92b4a3d0e43c8ea9fa39e0194890
jason1642 <jason1642@gmail.com>
2023-04-05 14:25:35 -0400

    Site Header component

--- src/components/Header/SiteHeader.tsx
+++ src/components/Header/SiteHeader.tsx
@@ -5,35 +5,59 @@ import Image from 'next/image';
 import Link from 'next/link';
 import './SiteHeader.css'
 import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+
 
 interface ISiteHeaderProps {
 }
 
+const LinkContainer = styled(Link)`
+  display: flex;
+  height: inherit;
+  align-items: center;
+
+  /* width: 100%; */
+`
+
+const Title = styled.div`
+  font-size: 2.1em;
+  font-weight: lighter;
+`;
 const Container = styled.header`
-  
+  display: flex;
+`;
+
+const SearchBox = styled.div`
+  display:flex;
+  justify-content: center;
 `;
 
 const SiteHeader: React.FunctionComponent<ISiteHeaderProps> = (props) => {
   return (
     <Container>
- <Link className="home-link" href="/">
+ <LinkContainer href="/">
       <Image
         
         alt='logo'
-        width={90}
-        height={90}
-        className="header-img"
+        width={75}
+        height={75}
+        // className="header-img"
         src="https://cdn3.iconfinder.com/data/icons/abstract-tree-wood-forest-ecology-landscape-eco-gr/1470/green_eco_leaves_circle_logo_eco_recycle-512.png" />
-     
-      Home</Link>
+      <Title>Next News Hub</Title>
+      </LinkContainer>
 
-      
-      <h1 className="header-title">Header</h1>
+      <SearchBox>
+              <TextField id="outlined-basic" label="Search" variant="outlined" />
 
-
-      <input className="search-box" type="text" placeholder="Search" />
+      {/* <input className="search-box" type="text" placeholder="Search" /> */}
       {/* <Button className="search-button">I&apos;m feeling lucky!</Button> */}
-      <Button className="search-button">Search</Button>
+      <Button 
+        size='medium'
+        variant='contained'
+      >Search</Button>
+      </SearchBox>
+
+
      
 
         
