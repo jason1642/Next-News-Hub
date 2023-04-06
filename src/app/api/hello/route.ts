31e43e386b1ad97d6232962b2db113dc09a41cda
jason1642 <jason1642@gmail.com>
2023-04-05 22:25:21 -0400

    Nextjs API

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,3 +1,14 @@
-export async function GET(request: Request) {
-  return new Response('Hello, Next.js!')
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+
+// export async function GET(req: NextApiRequest, res:NextApiResponse) {
+//   // console.log('Thhihs is the handler api')
+
+//   return new Response('Hello, Next.js!')
+// }
+export default function GET(req: NextApiRequest, res: NextApiResponse) {
+  // const { pid } = req.query
+  console.log(req)
+  console.log('Thhihs is the handler api2')
+  res.send({message: 'this is a message'})
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
 'use client'
+import {useEffect, useState} from 'react'
 import NewsColumnOne from '@/components/home-page/NewsColumnOne'
 import { Inter } from 'next/font/google'
 import styles from './page.module.css'
+// import handler, { GET } from './api/hello/route'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+
+  useEffect(()=>{ 
+    fetch('/api/hello').then(res => console.log());
+  },[])
   return (
     <main className={styles.main}>
       <NewsColumnOne />
