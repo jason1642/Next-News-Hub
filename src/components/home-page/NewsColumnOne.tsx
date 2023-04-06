2e3ca6ddaed030049efc02c43dc3ab3c5cb5a888
jason1642 <jason1642@gmail.com>
2023-04-06 00:05:05 -0400

    News Column 1

--- src/components/home-page/NewsColumnOne.tsx
+++ src/components/home-page/NewsColumnOne.tsx
@@ -12,7 +12,7 @@ const NewsColumnOne: React.FunctionComponent<{newsData: any}> = ({newsData}) => 
 
 
     return (
-        <div className="flex p-0 flex-column align-middle">
+        <div className="flex flex-column flex-col align-middle">
 
 
 
@@ -21,27 +21,26 @@ const NewsColumnOne: React.FunctionComponent<{newsData: any}> = ({newsData}) => 
 
 
     
-        
+        {/* Main News(Single) Container */}
         <div
          onClick={()=>window.open(newsData[0].url, '_blank')}
-         className="main-story-div w-6/12 flex flex-col align-middle border-b border-black border-solid"
+         className="w-6/12 flex-col flex  align-middle border-b border-black border-solid p-2"
          >
 
  
-
+            {/* Title */}
             <h1 className="text-center text-4xl hover:cursor-pointer hover:text-blue-500">{newsData[0].title}</h1>
 
             <Image 
-            style={{height: 'auto !important', width: '90% !important'}}
             className="h-auto w-11/12"src={newsData[0].multimedia[0].url}
              alt='something' 
-             
              width={newsData[0].multimedia[0].width}
              height={newsData[0].multimedia[0].height}
              />
-<p className="col-one-main-desc px-5 hover:cursor-pointer hover:text-blue-500">{newsData[0].abstract}</p>
+      <p className="col-one-main-desc px-5 hover:cursor-pointer hover:text-blue-500">{newsData[0].abstract}</p>
         </div>
-        <div className="col-one-link-section">
+        {/* col-one-link-section  */}
+        <div className="flex flex-col align-middle w-11/12">
             {newsData.map((ele:any) => {
                     if(ele.subsection !== ''){    
                     let tempSubSec = ele.subsection.split("");
