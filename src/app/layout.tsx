85f3410ce08956bc57697eee33c726d0934b633a
jason1642 <jason1642@gmail.com>
2023-04-06 23:47:11 -0400

    Weather dashboard components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 
 import './globals.css'
 import Header from '../components/Header/SiteHeader'
+// import ReactRain from 'react-rain-animation'
+// import "react-rain-animation/lib/style.css";
 export const metadata = {
   title: 'Next News Hub',
   description: 'Welcome to Next News Hub',
@@ -16,7 +18,9 @@ export default function RootLayout({
 <head>
 {/* <link href="/dist/output.css" rel="stylesheet"/> */}
 </head>
-      <body className='bg-white h-'>
+      <body className='bg-black h-'>
+      {/* <ReactRain numDrops='50'/> */}
+
         <Header />
         {children}
         </body>
--- src/components/weather-section/BasicDataPanel.tsx
+++ src/components/weather-section/BasicDataPanel.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 
+
 interface IBasicDataPanelProps {
     weatherData: any;
     location: string;
@@ -11,7 +12,7 @@ const BasicDataPanel: React.FunctionComponent<IBasicDataPanelProps> = ({weatherD
           [location]:{ 
             address,
             currentConditions:{
-              datetime
+              datetime, temp
             }
           }
         }
@@ -19,7 +20,6 @@ const BasicDataPanel: React.FunctionComponent<IBasicDataPanelProps> = ({weatherD
       console.log(weatherData)
       return (
         <div className='rounded-xl basis-1/3 p-4 font-light text-white bg-blue-400'>
-          
            {/* Location + sunrise */}
             {/* <div className='flex justify-between'>
                 <span className='text-md'>{address.split(',')[0]}</span>
@@ -30,10 +30,13 @@ const BasicDataPanel: React.FunctionComponent<IBasicDataPanelProps> = ({weatherD
             <span className='text-md font-extralight'>Today {Intl.DateTimeFormat('en', { hour: "numeric", minute: "numeric", hour12: true }).format(new Date(datetime))} EST</span>
             <span className='text-xsm'>8:06 pm</span>
        </div> */}
-            <h3 className='font-extralight text-4xl'>{address.split(',')[0]}</h3>
+            <h3 className='font-extralight text-4xl text-center'>{address.split(',')[0]}</h3>
+            <p className='font-extralight text-6xl text-center'>{Math.round(temp)}&deg;</p>
 
-
-
+            {/* <canvas id="canvas" style={{position : 'absolute', top: '0px', left : '0px'}}></canvas>
+<script type="text/javascript" src="http://sheepeuh.com/rain/dat.gui.js"></script>
+<div style={{position : 'absolute', bottom : '0px', left : '0px', margin : '20px', padding : '5px', backgroundColor:'#ce3635'}}>
+</div> */}
             {/* Location, sunrise/sunset times w/icons */}
             {/* 'As of' Date */}
             {/* Temperature with condition(ex:Sunny w/icon) */}
