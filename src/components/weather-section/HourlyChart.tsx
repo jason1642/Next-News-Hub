29512ce67567d3a4fc6faaa8e5409230fc24625f
jason1642 <jason1642@gmail.com>
2023-04-17 15:39:11 -0400

    Chart xaxis alignments

--- src/components/weather-section/HourlyChart.tsx
+++ src/components/weather-section/HourlyChart.tsx
@@ -10,11 +10,14 @@ interface IHourlyChartProps {
 
 const RenderTickIcon = ({ x, y, payload }: any)=>{
     console.log(payload)
-    return <g transform={`translate(${x},${y - 40})`}>
+    return <g 
+    style={{margin: '0 auto'}}
+    transform={`translate(${x - 20},${y - 40})`}
+    >
         <image
             xlinkHref={`https://raw.githubusercontent.com/visualcrossing/WeatherIcons/main/PNG/4th%20Set%20-%20Color/${payload.value}.png`}
-            height="23px"
-            width="44px"
+            height="30px"
+            // width="44px"
         />
     </g>
 }
@@ -40,84 +43,93 @@ const HourlyChart: React.FunctionComponent<IHourlyChartProps> = ({weatherData}) 
   }, [hourlyWeatherData]);
 
   return (
-    <div>
-        { hourlyWeatherData && 
+      <div style={{ margin: '0 auto' }}>
+          {hourlyWeatherData &&
 
-        <AreaChart 
-        width={700} 
-        height={250}
-        data={hourlyWeatherData}
-         >
+              <AreaChart
+                  width={700}
+                  height={250}
+                  data={hourlyWeatherData}
+                  margin={{
+                    right: 30
+                  }}
+              >
 
-            <CartesianGrid stroke={'#666'} horizontal={false}/>
+                  <CartesianGrid stroke={'#666'} horizontal={false} />
 
-            <Area xAxisId={1}  type="natural" dataKey="tempo" stroke="#8884d8" />
-            <XAxis
-                tickCount={9}
-                interval={0}
-                // allowDuplicatedCategory={true} 
-                allowDataOverflow={true} 
-                xAxisId={1}
-                tickLine={false}  
-                type='category' 
-                axisLine={false}
-                tickFormatter={(val)=>`${val}\u00b0`}
-                dataKey={'temp'} 
-                orientation='top'
-                tick={{fontSize: '1.1rem',}} 
-                />
+                  <Area hide={true} xAxisId={1} type="natural" dataKey="temp" stroke="#8884d8" />
+                  <XAxis
+                      interval={0}
+                      // allowDuplicatedCategory={true} 
+                    //   allowDataOverflow={true}
+                      xAxisId={1}
+                      tickLine={false}
+                      type='category'
+                      axisLine={false}
+                      tickFormatter={(val) => `${val}\u00b0`}
+                      dataKey={'temp'}
+                      orientation='top'
+                      tick={{ fontSize: '1.1rem', }}
+                  />
 
-            <Area xAxisId={2} type="natural" dataKey="temp" stroke="#8884d8" />
-                <XAxis
-                interval={0}
-                tickCount={9}
-                tick={<RenderTickIcon/>}
-                height={60}
-                // width={10}
-                allowDuplicatedCategory={true} 
-                allowDataOverflow={true} 
-                xAxisId={2}
-                tickLine={false}  
-                // type='category' 
-                axisLine={false}
-            //   tickFormatter={(val)=>`${val}`}
-                dataKey={'icon'} 
-                orientation='top'
-                />
-
-
-
-
-
-
-        <Area xAxisId={3} type="natural" dataKey="condition" stroke="#8884d8" />
-            <XAxis
-            allowDuplicatedCategory={true} 
-            allowDataOverflow={true} 
-            type="category"
-            xAxisId={3} 
-            tickLine={false}
-            axisLine={false} 
-            dataKey="dateTime"
-            orientation='top'  
-            tick={{fontSize: '1.2rem',}} 
-            interval={0}
-            />
+                  <Area xAxisId={2} type="natural" dataKey="temp" stroke="#8884d8" />
+                  <XAxis
+                      interval={0}
+                      tick={<RenderTickIcon />}
+                      height={50}
+                      // width={10}
+                    //   allowDuplicatedCategory={true}
+                    //   allowDataOverflow={true}
+                      xAxisId={2}
+                      tickLine={false}
+                      // type='category' 
+                      axisLine={false}
+                      //   tickFormatter={(val)=>`${val}`}
+                      dataKey={'icon'}
+                      orientation='top'
+                  />
 
 
 
 
 
-            <CartesianAxis stroke="#060606"  />
+
+                  <Area hide={true} xAxisId={3} type="natural" dataKey="condition" stroke="#8884d8" />
+                  <XAxis
+                    //   allowDuplicatedCategory={true}
+                    //   allowDataOverflow={true}
+                    // hide
+                      type="category"
+                      xAxisId={3}
+                      tickLine={false}
+                      axisLine={false}
+                      dataKey="dateTime"
+                      orientation='top'
+                      tick={{ fontSize: '1rem', }}
+                      interval={0}
+                  />
 
 
 
 
-            <YAxis hide={true} allowDecimals={false} tick={{fontSize: '.8rem'}} type='number' domain={['dataMin - 6', 'dataMax + 6']}/>
-            <Tooltip />
-        </AreaChart>
-        }
-    </div>
+
+                  <CartesianAxis stroke="#060606" />
+
+
+
+
+                  <YAxis
+                      axisLine={false}
+                      allowDecimals={false}
+                      tick={false}
+                    //   hide
+                    //   type='number'
+                    //   domain={['dataMin - 6', 'dataMax + 6']}
+                  />
+                  <Tooltip />
+              </AreaChart>
+          }
+      </div>
   );
 };
 
